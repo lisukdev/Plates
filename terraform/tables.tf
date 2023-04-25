68d184451e853c97e329a6e4c5cd95a4cfd5e5ea
David Lisuk <david@lisuk.dev>
2023-04-25 15:46:26 -0400

    Fixing teraform formatting

--- terraform/tables.tf
+++ terraform/tables.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "tf_notes_table" {
-  name = "tf-notes-table"
+  name         = "tf-notes-table"
   billing_mode = "PAY_PER_REQUEST"
   attribute {
     name = "noteId"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,21 +1,21 @@
 variable "aws_region" {
-  type = string
+  type        = string
   description = "The region in which the resources will be created"
-  default = "us-east-2"
+  default     = "us-east-2"
 }
 
 variable "AWS_ACCESS_KEY_ID" {
-  type = string
+  type        = string
   description = "The aws development account access key"
 }
 
 variable "AWS_SECRET_ACCESS_KEY" {
-  type = string
+  type        = string
   description = "The aws development account secret key"
 }
 
 variable "app_name" {
-  type = string
+  type        = string
   description = "The name of the application"
-  default = "plates"
+  default     = "plates"
 }
