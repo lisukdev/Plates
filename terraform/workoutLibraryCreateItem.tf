68454391d5c6f555a234543d5a52ed5d2cbe0baf
David Lisuk <david@lisuk.dev>
2023-04-26 16:09:22 -0400

    Fixing typo

--- terraform/workoutLibraryCreateItem.tf
+++ terraform/workoutLibraryCreateItem.tf
@@ -18,7 +18,7 @@ resource "aws_lambda_function" "workoutLibraryCreateItem" {
 resource "aws_lambda_permission" "workoutLibraryCreateItem" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_workoutLibraryCreateItem.workoutLibraryCreateItem.workoutLibraryCreateItem_name
+  function_name = aws_lambda_function.workoutLibraryCreateItem.function_name
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
