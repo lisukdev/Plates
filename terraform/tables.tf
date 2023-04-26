899a4c21ef6cfbb62c094d477e8a795e54d03f49
David Lisuk <david@lisuk.dev>
2023-04-26 17:21:16 -0400

    Modify dynamo keys to support go structs

--- terraform/tables.tf
+++ terraform/tables.tf
@@ -1,10 +1,10 @@
 resource "aws_dynamodb_table" "workout_templates" {
   name         = "WorkoutTemplates"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "id"
+  hash_key     = "Id"
 
   attribute {
-    name = "id"
+    name = "Id"
     type = "S"
   }
 }
@@ -12,15 +12,15 @@ resource "aws_dynamodb_table" "workout_templates" {
 resource "aws_dynamodb_table" "user_workout_templates" {
   name         = "UserWorkoutTemplates"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "userId"
-  range_key    = "templateId"
+  hash_key     = "UserId"
+  range_key    = "TemplateId"
 
   attribute {
-    name = "userId"
+    name = "UserId"
     type = "S"
   }
   attribute {
-    name = "templateId"
+    name = "TemplateId"
     type = "S"
   }
 }
